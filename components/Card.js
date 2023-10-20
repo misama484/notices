6295a3e0c2a29bda5764a2ab72fe407862807d8d
Miguelsanchez.dev <info@miguelsanchez.dev>
2023-10-20 10:14:34 +0200

    preparando modal Card

--- components/Card.js
+++ components/Card.js
@@ -1,34 +1,31 @@
 import { useState, useEffect } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import { Modal } from 'react-native-paper';
 
 
 const url = "http://api.mediastack.com/v1/news?access_key=4871737edbb903eb46aa728fec1a155f&languages=es&country=es&categories=science"
 
 
-const Card = () => {
-
-const [notices, setNotices] = useState([]);
-
-useEffect(() => {
-  fetchNotices();
- },[]);
-
-const fetchNotices = () => {
-  fetch('http://api.mediastack.com/v1/news?access_key=4871737edbb903eb46aa728fec1a155f&languages=es&country=es&categories=science')
-  .then(response => response.json())
-  .then(notices => setNotices(notices));
-  };
-
-
+const Card = ({
+  modalVisible,
+  setModalVisible,
+  notice,
+}) => {
 
   return (
-    <View>
-     {notices.map((notice) => {            
-        return (                  
-          <Text>{notice.data.title}</Text>
-            );
-      })}
-    </View>
+    <Modal
+      modalVisible={modalVisible}
+      onDismiss={() => setModalVisible(false)}
+      animationType = "slide"
+      transparent = {true}
+      >
+      <ScrollView>
+        <View>
+          <Text>{notice.title}</Text>
+          <Text>{notice.description}</Text>
+        </View>
+      </ScrollView>
+    </Modal>
   )
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
-import { View, Text, Image } from "react-native"
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native"
 
 import Card from "../components/Card"
 
 
 
-const Home = () => {
+const Home = (navigation) => {
 
 const [notices, setNotices] = useState([]);
+const [notice, setNotice] = useState({});
+const [modalVisible, setModalVisible] = useState(false);
 
 useEffect(() => {
   fetchNotices();
@@ -16,18 +18,36 @@ useEffect(() => {
 const fetchNotices = () => {
   fetch('http://api.mediastack.com/v1/news?access_key=4871737edbb903eb46aa728fec1a155f&languages=es&country=es&categories=science')
   .then(response => response.json())
-  .then(notices => setNotices(notices));
+  .then(notices => setNotices(notices.data));
   };
 
 
 return (
-  <View>
-    {notices.map((notice) => {            
-      return (                  
-        <Text>{notice.data.title}</Text>
-          );
-    })}
-  </View>
+  <ScrollView>
+    <View>
+      {notices.map((notice, index) => {            
+        return (               
+          <TouchableOpacity
+            key={index}
+            onPress={() => {
+              setModalVisible(!modalVisible)
+              setNotice(notice);
+            }}   
+            >
+              <Image
+              style={{width: 450, height: 280, borderRadius: 20}}
+              src={notice.image}
+              />
+              <Text>{notice.title}</Text>
+            </TouchableOpacity>
+            );
+      })}
+      <Card
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        notice={notice} />
+    </View>
+  </ScrollView>
   )
 }
 
