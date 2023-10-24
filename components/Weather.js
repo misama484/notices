07b7e360111047a5958fc0429fa75ff3f40bb539
Miguelsanchez.dev <info@miguelsanchez.dev>
2023-10-24 11:06:48 +0200

    intentando asignacion de ciudad

--- components/Weather.js
+++ components/Weather.js
@@ -9,6 +9,8 @@ const Search = () => {
 
   const [expanded, setExpanded] = useState(false)
   const [city, setCity] = useState("")
+  const [ciudad, setCiudad] = useState("")
+  
   useEffect(() => {
     fetchWeather();
    },[]);
@@ -17,21 +19,28 @@ const Search = () => {
     const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&appid=0a6b47b5eeda727c6b9d969e42c82b6b&lang=es&q' + city
     fetch(apiUrl)
     .then(response => response.json())
-    .then(notices => setNotices(notices.data));
+    .then(city => setCity(city));
     };
+    
+    
 
+    const handleCity = (ciudad) => {
+      setCiudad(ciudad)
+      console.log(city)
+    }
   return(
     <View>
      <TextInput
       label="Ciudad"
-      
+      value = {city}
+      onChangeText={city => handleCity(city)}
       
     />
     <Button
       mode="outlained"
-      onPress={() => {setCity(city)}}
+      onPress={() => {setCity(ciudad)}}
     >Buscar</Button>
-    <Text>{city}</Text>
+    <Text>{city.base}</Text>
     </View>
   )
 
