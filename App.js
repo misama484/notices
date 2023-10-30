14ae4843d716a4bcb35caaf30c04319417a115d5
Miguel Sanchez Martínez <86913656+misama484@users.noreply.github.com>
2023-10-30 09:49:06 +0100

    cambiando de equipo, instalando dependencias

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4630AB',
     alignItems: 'center',
     justifyContent: 'center',
   },
